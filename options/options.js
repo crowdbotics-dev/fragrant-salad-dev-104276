1805c072db46e7d6256270fa4a3c32cd665af9c1
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 08:17:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-salad-dev-104276",
+  url: "https://fragrant-salad-dev-104276.botics.co",
+  api: "https://fragrant-salad-dev-104276.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
